fc95f1377594134460e11a5f879e43c587557952
Yasar icli <hello@yasaricli.com>
2015-04-01 10:10:53 +0300

    jsnes licenses add jsnes.js

--- client/lib/jsnes.js
+++ client/lib/jsnes.js
@@ -1,3 +1,18 @@
+/*
+JSNES, based on Jamie Sanders' vNES
+Copyright (C) 2010 Ben Firshman
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 var root = this;
 
 /*
@@ -6697,69 +6712,4 @@ JSNES.ROM.prototype = {
             return null;
         }
     }
-};
-/* ROM.JS END */
-
-JSNES.DummyUI = function(nes) {
-    this.nes = nes;
-    this.enable = function() {};
-    this.updateStatus = function() {};
-    this.writeAudio = function() {};
-    this.writeFrame = function() {};
-};
-
-root.Jnes = function(_this) {
-    return new JSNES({
-        ui: function(nes) {
-
-            this.screen = _this.find('#Emulator');
-            this.canvasContext = this.screen.getContext('2d');
-            this.canvasImageData = this.canvasContext.getImageData(0, 0, 256, 240);
-
-            for (var i = 3; i < this.canvasImageData.data.length-3; i += 4) {
-                this.canvasImageData.data[i] = 0xFF;
-            }
-
-            // KEYBOARD
-            $(document).
-                bind('keydown', function(evt) {
-                      nes.keyboard.keyDown(evt);
-                }).
-                bind('keyup', function(evt) {
-                    nes.keyboard.keyUp(evt);
-                }).
-                bind('keypress', function(evt) {
-                    nes.keyboard.keyPress(evt);
-            });
-
-            this.enable = function() {};
-            this.updateStatus = function(text) {
-                Session.set('status', text);
-            };
-
-            this.screenshot = function() {
-                var data = this.screen.toDataURL("image/png"),
-                    img = new Image();
-                img.src = data;
-                return img;
-            };
-
-            this.writeAudio = function() {};
-            this.writeFrame = function(buffer, prevBuffer) {
-                var imageData = this.canvasImageData.data,
-                    pixel, i, j;
-                for (i=0; i<256*240; i++) {
-                    pixel = buffer[i];
-                    if (pixel != prevBuffer[i]) {
-                        j = i*4;
-                        imageData[j] = pixel & 0xFF;
-                        imageData[j+1] = (pixel >> 8) & 0xFF;
-                        imageData[j+2] = (pixel >> 16) & 0xFF;
-                        prevBuffer[i] = pixel;
-                    }
-                }
-                this.canvasContext.putImageData(this.canvasImageData, 0, 0);
-            };
-        }
-    });
 };
--- client/lib/shortcuts.js
+++ client/lib/shortcuts.js
@@ -3,3 +3,70 @@ var root = this;
 root.isAuthenticated = function(callback, $this) {
     return callback && callback.call($this ? $this : {});
 }
+
+
+/* ROM.JS END */
+
+JSNES.DummyUI = function(nes) {
+    this.nes = nes;
+    this.enable = function() {};
+    this.updateStatus = function() {};
+    this.writeAudio = function() {};
+    this.writeFrame = function() {};
+};
+
+root.Jnes = function(_this) {
+    return new JSNES({
+        ui: function(nes) {
+
+            this.screen = _this.find('#Emulator');
+            this.canvasContext = this.screen.getContext('2d');
+            this.canvasImageData = this.canvasContext.getImageData(0, 0, 256, 240);
+
+            for (var i = 3; i < this.canvasImageData.data.length-3; i += 4) {
+                this.canvasImageData.data[i] = 0xFF;
+            }
+
+            // KEYBOARD
+            $(document).
+                bind('keydown', function(evt) {
+                      nes.keyboard.keyDown(evt);
+                }).
+                bind('keyup', function(evt) {
+                    nes.keyboard.keyUp(evt);
+                }).
+                bind('keypress', function(evt) {
+                    nes.keyboard.keyPress(evt);
+            });
+
+            this.enable = function() {};
+            this.updateStatus = function(text) {
+                Session.set('status', text);
+            };
+
+            this.screenshot = function() {
+                var data = this.screen.toDataURL("image/png"),
+                    img = new Image();
+                img.src = data;
+                return img;
+            };
+
+            this.writeAudio = function() {};
+            this.writeFrame = function(buffer, prevBuffer) {
+                var imageData = this.canvasImageData.data,
+                    pixel, i, j;
+                for (i=0; i<256*240; i++) {
+                    pixel = buffer[i];
+                    if (pixel != prevBuffer[i]) {
+                        j = i*4;
+                        imageData[j] = pixel & 0xFF;
+                        imageData[j+1] = (pixel >> 8) & 0xFF;
+                        imageData[j+2] = (pixel >> 16) & 0xFF;
+                        prevBuffer[i] = pixel;
+                    }
+                }
+                this.canvasContext.putImageData(this.canvasImageData, 0, 0);
+            };
+        }
+    });
+};
