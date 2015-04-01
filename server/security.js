98e7f7d674896437457d85aa5c61ef96781c9b63
Yasar icli <hello@yasaricli.com>
2015-04-01 16:23:01 +0300

    Roms collection update ifLoggedIn and onlyProps apply

--- server/security.js
+++ server/security.js
@@ -1,20 +1,6 @@
-/*
- *
- * Call Security.defineMethod to define a method that may be used in the rule
- * chain to restrict the currect rule.
- * Pass a definition argument, which must contain a deny property
- * set to a deny function for that rule.
- * */
-Security.defineMethod("ifUpdateRom", {
-    deny: function(type, arg, userId, doc, fields, filter) {
-        var allowKeys = ['stars'];
-        return contains(_.keys(_.omit(doc, allowKeys)), fields);
-    }
-});
-
 // ROMS COLLECTION
 Roms.permit(['insert', 'remove']).ifHasRole('admin').apply();
-Roms.permit('update').ifUpdateRom().apply();
+Roms.permit('update').ifLoggedIn().onlyProps(['stars']).apply();
 
 // FILES COLLECTION
 Files.files.permit(['insert', 'remove', 'update']).ifHasRole('admin').apply();
